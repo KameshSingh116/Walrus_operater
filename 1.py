62bc1c449a003a2a7514e38e9c508d21bcaf1176
Kamesh Singh <radhakrishnapg152627@gmail.com>
2025-01-05 23:57:11 +0530

    end

--- 1.py
+++ 1.py
@@ -1,3 +1,5 @@
 #Radhe Radhe
 if(n:=len([1,2,3,4,5,6]))>3:
     print(f"List is too long ({n} elements, expected <=3)")
+
+#Using this we can assign value to a variable while using it in an expression.
